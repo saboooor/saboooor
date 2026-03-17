eaf9f77c1c0774e659a311de103fe6f4fbc6c92b
saboooor <saboor547@gmail.com>
2026-03-17 01:13:51 -0300

    fix: adjust layout of nav component for improved alignment and aesthetics

--- saboor.ca/src/components/Nav.tsx
+++ saboor.ca/src/components/Nav.tsx
@@ -6,7 +6,7 @@ export const Nav = component$(() => {
   const loc = useLocation();
 
   return <nav class="fixed bottom-4 right-4 flex z-10" style={{ '--lum-border-radius': '0.8rem' }}>
-    <div class="lum-card flex-row mx-auto lum-bg-gray-900/50 backdrop-blur-lg p-1 gap-1 flex items-center">
+    <div class="lum-card lum-bg-gray-900/50 backdrop-blur-lg p-1 gap-1 flex">
       <Link href="/" class={{
         'lum-btn lum-btn-p-1 lum-bg-transparent rounded-lum-1': true,
         'lum-bg-luminescent-500/20 hover:lum-bg-luminescent-600': loc.url.pathname === '/',
