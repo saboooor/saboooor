9b3dc873a9d6faf5e6a32cd87f89639caae3fd9b
saboooor <github@saboor.ca>
2026-07-21 20:57:14 -0400

    h

--- saboor.ca/src/components/Activity/ActivityCard.tsx
+++ saboor.ca/src/components/Activity/ActivityCard.tsx
@@ -1,19 +1,26 @@
-import { component$, PropsOf, Signal, useContext, useSignal } from "@qwik.dev/core";
-import { activityTypes, convertTime } from "./Lanyard";
-import { getClassObject } from "@luminescent/ui-qwik";
-import X from "lucide-icons-qwik/icons/X";
-import { NowContext } from "~/routes/layout";
+import {
+  component$,
+  PropsOf,
+  Signal,
+  useContext,
+  useSignal,
+} from '@qwik.dev/core';
+import { activityTypes, convertTime } from './Lanyard';
+import { getClassObject } from '@luminescent/ui-qwik';
+import X from 'lucide-icons-qwik/icons/X';
+import { NowContext } from '~/routes/layout';
 
-interface ActivityCardProps extends PropsOf<"div"> {
+interface ActivityCardProps extends PropsOf<'div'> {
   activity: any;
-  modalRef?: Signal<HTMLDialogElement>;
+  modalRef?: Signal<HTMLDialogElement | undefined>;
   fixedwidth?: boolean;
   compact?: boolean;
 }
 
 export default component$<ActivityCardProps>(
   ({ activity, fixedwidth, compact, class: Class, ...props }) => {
-    const activityType = activityTypes[activity.type as keyof typeof activityTypes];
+    const activityType =
+      activityTypes[activity.type as keyof typeof activityTypes];
     const modalRef = useSignal<HTMLDialogElement>();
     const now = useContext(NowContext);
 
@@ -21,9 +28,9 @@ export default component$<ActivityCardProps>(
       <div
         key={activity.id}
         class={{
-          "lum-card lum-grad-bg-gray-950/80 rounded-lum-2 relative p-2 transition-all duration-300": true,
-          "min-w-full flex-1 md:max-w-2/3 md:min-w-1/3": !fixedwidth,
-          "w-80": fixedwidth,
+          'lum-card lum-grad-bg-gray-950/80 rounded-lum-2 relative p-2 transition-all duration-300': true,
+          'min-w-full flex-1 md:max-w-2/3 md:min-w-1/3': !fixedwidth,
+          'w-80': fixedwidth,
           ...getClassObject(Class),
         }}
         {...props}
@@ -33,11 +40,11 @@ export default component$<ActivityCardProps>(
           onClick$={() => {
             modalRef.value?.showModal();
           }}
-          aria-label={"Expand"}
+          aria-label={'Expand'}
         />
         <div class="rounded-lum-2 absolute inset-0 -z-10 h-full w-full overflow-clip object-cover saturate-200">
           <img
-            class="absolute inset-0 -translate-y-1/3 animate-spin saturate-200 animation-duration-[10s]"
+            class="animation-duration-[10s] absolute inset-0 -translate-y-1/3 animate-spin saturate-200"
             src={activity.assets?.large_image}
             alt={activity.assets?.large_text}
             width={400}
@@ -47,27 +54,29 @@ export default component$<ActivityCardProps>(
         <div class="rounded-lum-2 absolute inset-0 -z-10 backdrop-blur-lg" />
         <div
           class={{
-            "group absolute top-2 right-2 z-2 flex items-center gap-2": true,
+            'group absolute top-2 right-2 z-2 flex items-center gap-2': true,
           }}
         >
           <p
             class={{
-              "lum-btn lum-grad-bg-gray-900/50 lum-btn-p-1 pointer-events-none absolute top-0 right-7 -z-1 -translate-x-2 text-xs whitespace-nowrap opacity-0 backdrop-blur-sm group-hover:pointer-events-auto group-hover:translate-x-0 group-hover:opacity-100": true,
+              'lum-btn lum-grad-bg-gray-900/50 lum-btn-p-1 pointer-events-none absolute top-0 right-7 -z-1 -translate-x-2 text-xs whitespace-nowrap opacity-0 backdrop-blur-sm group-hover:pointer-events-auto group-hover:translate-x-0 group-hover:opacity-100': true,
             }}
           >
             {activityType?.text} <b>{activity.name}</b>
           </p>
-          {activityType?.icon && <activityType.icon size={24} class="lum-btn p-1" />}
+          {activityType?.icon && (
+            <activityType.icon size={24} class="lum-btn p-1" />
+          )}
         </div>
         <div
           class={{
-            "z-1 my-auto flex flex-row items-center gap-2": true,
+            'z-1 my-auto flex flex-row items-center gap-2': true,
           }}
         >
           {activity.assets?.large_image && (
             <div
               class={{
-                "lum-grad-bg-yellow-500/0 rounded-lum-4 relative mb-auto h-16 w-16": true,
+                'lum-grad-bg-yellow-500/0 rounded-lum-4 relative mb-auto h-16 w-16': true,
               }}
             >
               <img
@@ -76,7 +85,7 @@ export default component$<ActivityCardProps>(
                 width={80}
                 height={80}
                 class={{
-                  "rounded-lum-4 absolute top-0 -z-1": true,
+                  'rounded-lum-4 absolute top-0 -z-1': true,
                 }}
               />
               {activity.assets?.small_image && (
@@ -91,9 +100,13 @@ export default component$<ActivityCardProps>(
             </div>
           )}
           <div class="flex flex-1 flex-col text-xs">
-            {activity.details && <p class="font-semibold">{activity.details}</p>}
+            {activity.details && (
+              <p class="font-semibold">{activity.details}</p>
+            )}
             {activity.state && (
-              <p class="overflow-hidden text-ellipsis text-gray-400">{activity.state}</p>
+              <p class="overflow-hidden text-ellipsis text-gray-400">
+                {activity.state}
+              </p>
             )}
             {activity.assets?.large_text && (
               <p class="text-gray-400">{activity.assets.large_text}</p>
@@ -127,122 +140,131 @@ export default component$<ActivityCardProps>(
         <dialog
           ref={modalRef}
           class={{
-            "text-lum-text m-auto hidden overflow-visible open:flex": true,
-            "max-w-2/3 bg-transparent drop-shadow-2xl lg:max-w-1/2 2xl:max-w-1/3": true,
-            "open:animate-in open:fade-in open:slide-in-from-top-8 open:duration-300": true,
-            "animate-out fade-out slide-in-from-top-8 duration-300": true,
+            'text-lum-text m-auto hidden overflow-visible open:flex': true,
+            'max-w-2/3 bg-transparent drop-shadow-2xl lg:max-w-1/2 2xl:max-w-1/3': true,
+            'open:animate-in open:fade-in open:slide-in-from-top-8 open:duration-300': true,
+            'animate-out fade-out slide-in-from-top-8 duration-300': true,
           }}
-        ></dialog>
+        >
+          <ExpandedCard activity={activity} modalRef={modalRef} />
+        </dialog>
       </div>
     );
-  },
+  }
 );
 
-export const ExpandedCard = component$<ActivityCardProps>(({ activity, modalRef }) => {
-  const activityType = activityTypes[activity.type as keyof typeof activityTypes];
-  const now = useContext(NowContext);
+export const ExpandedCard = component$<ActivityCardProps>(
+  ({ activity, modalRef }) => {
+    const activityType =
+      activityTypes[activity.type as keyof typeof activityTypes];
+    const now = useContext(NowContext);
 
-  return (
-    <div class="lum-card lum-grad-bg-gray-950/60 rounded-lum-2 relative h-full w-full p-12 transition-all duration-300">
-      <div class="rounded-lum-2 absolute inset-0 -z-10 h-full w-full overflow-clip object-cover saturate-200">
-        <img
-          class="absolute top-0 right-0 -translate-y-1/3 scale-150 animate-spin saturate-200 animation-duration-[15s]"
-          src={activity.assets?.large_image}
-          alt={activity.assets?.large_text}
-          width={1024}
-          height={1024}
-        />
-        <img
-          class="animate-spin-cc absolute bottom-0 left-0 translate-y-1/3 scale-150 saturate-200 animation-duration-[20s]"
-          src={activity.assets?.large_image}
-          alt={activity.assets?.large_text}
-          width={1024}
-          height={1024}
-        />
-      </div>
-      <div class="rounded-lum-2 absolute inset-0 -z-10 backdrop-blur-2xl" />
-
-      <div class="-mt-5 mb-3 flex">
-        <div
-          class={{
-            "top-2 right-2 z-2 flex flex-1 items-center gap-2": true,
-          }}
-        >
-          {activityType?.icon && <activityType.icon />}
-          <b>{activity.name}</b>
-        </div>
-        {modalRef && (
-          <button
-            class="rounded-lum-2 lum-btn lum-bg-transparent p-2"
-            onClick$={() => {
-              modalRef.value?.close();
-            }}
-            aria-label={"Close"}
-          >
-            <X />
-          </button>
-        )}
-      </div>
-
-      {activity.assets?.large_image && (
-        <a
-          class={{
-            "lum-grad-bg-yellow-500/0 rounded-lum-4 relative h-auto w-full": true,
-          }}
-          href={activity.assets.large_url}
-        >
+    return (
+      <div class="lum-card lum-grad-bg-gray-950/60 rounded-lum-2 relative h-full w-full p-12 transition-all duration-300">
+        <div class="rounded-lum-2 absolute inset-0 -z-10 h-full w-full overflow-clip object-cover saturate-200">
           <img
-            src={activity.assets.large_image.replace("128x128", "1024x1024")}
-            alt={activity.assets.large_text}
+            class="animation-duration-[15s] absolute top-0 right-0 -translate-y-1/3 scale-150 animate-spin saturate-200"
+            src={activity.assets?.large_image}
+            alt={activity.assets?.large_text}
             width={1024}
             height={1024}
-            class={{
-              "rounded-lum-4 top-0 -z-1 h-auto w-full": true,
-            }}
           />
-        </a>
-      )}
-      <div class="mt-7 flex flex-1 flex-col text-xs">
-        {activity.details && <p class="text-2xl font-semibold xl:text-4xl">{activity.details}</p>}
-        {activity.state && (
-          <p class="overflow-hidden text-xl text-ellipsis text-gray-400 xl:text-2xl">
-            {activity.state}
-          </p>
-        )}
-        {activity.assets?.large_text && (
-          <p class="text-lg text-gray-500 xl:text-xl">{activity.assets.large_text}</p>
-        )}
-        {activity.timestamps?.start && !activity.timestamps?.end && (
-          <p class="text-xl text-violet-300/50">
-            {convertTime(now.value - activity.timestamps.start)} elapsed
-          </p>
-        )}
-        {activity.timestamps?.end && !activity.timestamps?.start && (
-          <p class="text-xl text-violet-300/50">
-            {convertTime((now.value - activity.timestamps.end) * -1)} left
-          </p>
-        )}
-      </div>
-      {activity.timestamps?.start && activity.timestamps?.end && (
-        <div class="mt-7">
-          <div class="lum-grad-bg-gray-950/10 relative mb-1 min-h-4 overflow-x-clip rounded-full">
-            <div
-              class="absolute inset-0 rounded-full brightness-200 backdrop-saturate-200 transition-all duration-1000 ease-linear"
-              style={{
-                width: `${((now.value - activity.timestamps.start) / (activity.timestamps.end - activity.timestamps.start)) * 100}%`,
+          <img
+            class="animate-spin-cc animation-duration-[20s] absolute bottom-0 left-0 translate-y-1/3 scale-150 saturate-200"
+            src={activity.assets?.large_image}
+            alt={activity.assets?.large_text}
+            width={1024}
+            height={1024}
+          />
+        </div>
+        <div class="rounded-lum-2 absolute inset-0 -z-10 backdrop-blur-2xl" />
+
+        <div class="-mt-5 mb-3 flex">
+          <div
+            class={{
+              'top-2 right-2 z-2 flex flex-1 items-center gap-2': true,
+            }}
+          >
+            {activityType?.icon && <activityType.icon />}
+            <b>{activity.name}</b>
+          </div>
+          {modalRef && (
+            <button
+              class="rounded-lum-2 lum-btn lum-bg-transparent p-2"
+              onClick$={() => {
+                modalRef.value?.close();
+              }}
+              aria-label={'Close'}
+            >
+              <X />
+            </button>
+          )}
+        </div>
+
+        {activity.assets?.large_image && (
+          <a
+            class={{
+              'lum-grad-bg-yellow-500/0 rounded-lum-4 relative h-auto w-full': true,
+            }}
+            href={activity.assets.large_url}
+          >
+            <img
+              src={activity.assets.large_image.replace('128x128', '1024x1024')}
+              alt={activity.assets.large_text}
+              width={1024}
+              height={1024}
+              class={{
+                'rounded-lum-4 top-0 -z-1 h-auto w-full': true,
               }}
             />
-          </div>
-          <div class="mx-1 flex justify-between">
-            <p class="text-xl text-gray-400/50">
-              {convertTime(now.value - activity.timestamps.start)}
+          </a>
+        )}
+        <div class="mt-7 flex flex-1 flex-col text-xs">
+          {activity.details && (
+            <p class="text-2xl font-semibold xl:text-4xl">{activity.details}</p>
+          )}
+          {activity.state && (
+            <p class="overflow-hidden text-xl text-ellipsis text-gray-400 xl:text-2xl">
+              {activity.state}
             </p>
-            <p class="text-xl text-gray-400/50">
-              {convertTime((now.value - activity.timestamps.end) * -1)}
+          )}
+          {activity.assets?.large_text && (
+            <p class="text-lg text-gray-500 xl:text-xl">
+              {activity.assets.large_text}
             </p>
-          </div>
+          )}
+          {activity.timestamps?.start && !activity.timestamps?.end && (
+            <p class="text-xl text-violet-300/50">
+              {convertTime(now.value - activity.timestamps.start)} elapsed
+            </p>
+          )}
+          {activity.timestamps?.end && !activity.timestamps?.start && (
+            <p class="text-xl text-violet-300/50">
+              {convertTime((now.value - activity.timestamps.end) * -1)} left
+            </p>
+          )}
         </div>
-      )}
-    </div>
-  );
-});
+        {activity.timestamps?.start && activity.timestamps?.end && (
+          <div class="mt-7">
+            <div class="lum-grad-bg-gray-950/10 relative mb-1 min-h-4 overflow-x-clip rounded-full">
+              <div
+                class="absolute inset-0 rounded-full brightness-200 backdrop-saturate-200 transition-all duration-1000 ease-linear"
+                style={{
+                  width: `${((now.value - activity.timestamps.start) / (activity.timestamps.end - activity.timestamps.start)) * 100}%`,
+                }}
+              />
+            </div>
+            <div class="mx-1 flex justify-between">
+              <p class="text-xl text-gray-400/50">
+                {convertTime(now.value - activity.timestamps.start)}
+              </p>
+              <p class="text-xl text-gray-400/50">
+                {convertTime((now.value - activity.timestamps.end) * -1)}
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+);
